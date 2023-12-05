41b776cb0551e98e35cb30404ad49e08a11e43ce
Abraham Galue <abrahamgalue512@gmail.com>
2023-12-05 16:56:22 -0400

    feat: show restaurant list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="container m-auto grid min-h-screen grid-rows-[auto,1fr,auto] px-4">
-        <header className="text-xl font-bold leading-[3rem]">Restaurancy</header>
+        <header className="text-3xl text-rose-600 font-bold leading-[3rem] m-auto pt-8">
+          <h1>Restaurancy</h1>
+        </header>
         <main className="py-8">{children}</main>
         <footer className="text-center leading-[3rem] opacity-70">
           © {new Date().getFullYear()} Restaurancy
